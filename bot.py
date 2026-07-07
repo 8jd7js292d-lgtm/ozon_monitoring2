21ef353dff97bf5a4091c13e65be94c9f96262e9
8jd7js292d-lgtm <8jd7js292d@privaterelay.appleid.com>
2026-07-07 16:40:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,13 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 
-from database import init_db, add_product, get_products
+from database import (
+    init_db,
+    add_product,
+    get_products,
+    update_price
+)
+
 from price_checker import get_price
 
 
@@ -14,13 +20,14 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
-CHAT_ID = 401758093  
+CHAT_ID = 123456789  # замени на свой Telegram ID
 
 
 @dp.message()
 async def messages(message: Message):
 
     text = message.text
+
 
     if text.startswith("/add"):
 
@@ -36,6 +43,7 @@ async def messages(message: Message):
             "✅ Добавил товар в мониторинг 🔥"
         )
 
+
     elif text == "/check":
 
         products = get_products()
@@ -46,27 +54,70 @@ async def messages(message: Message):
             )
             return
 
+
         result = "🔎 Проверка цен:\n\n"
+
 
         for url, name, old_price in products:
 
             new_price = await get_price(url)
 
-            result += (
-                f"📦 {name}\n"
-                f"Старая цена: {old_price} ₽\n"
-                f"Новая цена: {new_price} ₽\n\n"
-            )
+
+            if old_price == 0:
+
+                update_price(
+                    url,
+                    new_price
+                )
+
+                result += (
+                    f"📦 {name}\n"
+                    f"Первая цена: {new_price} ₽\n\n"
+                )
+
+
+            elif new_price < old_price:
+
+                drop = round(
+                    (old_price - new_price)
+                    / old_price * 100
+                )
+
+
+                update_price(
+                    url,
+                    new_price
+                )
+
+
+                result += (
+                    f"🔥 СКИДКА!\n\n"
+                    f"📦 {name}\n"
+                    f"Было: {old_price} ₽\n"
+                    f"Стало: {new_price} ₽\n"
+                    f"Падение: -{drop}%\n\n"
+                )
+
+
+            else:
+
+                result += (
+                    f"📦 {name}\n"
+                    f"Цена: {new_price} ₽\n\n"
+                )
+
 
         await message.answer(result)
+
 
     else:
 
         await message.answer(
             "Команды:\n"
             "/add ссылка\n"
-            "/check проверка цен"
+            "/check проверка"
         )
+
 
 
 async def main():
@@ -75,10 +126,11 @@ async def main():
 
     await bot.send_message(
         CHAT_ID,
-        "🤖 Мониторинг запущен!"
+        "🤖 Мониторинг обновлён!"
     )
 
     await dp.start_polling(bot)
+
 
 
 if __name__ == "__main__":
