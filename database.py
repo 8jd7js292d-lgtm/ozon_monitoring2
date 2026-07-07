c1a94010f29b3b6886f904d9cfe66f5c99569135
8jd7js292d-lgtm <8jd7js292d@privaterelay.appleid.com>
2026-07-07 16:31:01 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+
+DB_NAME = "ozon.db"
+
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        url TEXT UNIQUE,
+        name TEXT,
+        price REAL,
+        updated TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_product(url, name, price):
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT OR REPLACE INTO products
+    (url, name, price, updated)
+    VALUES (?, ?, ?, datetime('now'))
+    """,
+    (url, name, price))
+
+    conn.commit()
+    conn.close()
+
+
+def get_products():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    SELECT url, name, price
+    FROM products
+    """)
+
+    data = cur.fetchall()
+
+    conn.close()
+
+    return data
