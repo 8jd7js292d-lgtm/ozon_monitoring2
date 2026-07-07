10a5eb5494f918c250edd648f45c8b2bbc940c4a
8jd7js292d-lgtm <8jd7js292d@privaterelay.appleid.com>
2026-07-07 16:03:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,40 @@
 import os
+import json
 import asyncio
+
 from aiogram import Bot
+from ozon_monitor import check_ozon
+
 
 TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = 401758093  
+
 
 async def main():
     bot = Bot(token=TOKEN)
+
+    with open("products.json", "r", encoding="utf-8") as f:
+        products = json.load(f)
+
+    message = "🔎 Проверка Ozon\n\n"
+
+    for product in products:
+        prices = await check_ozon(product["url"])
+
+        message += f"📦 {product['name']}\n"
+
+        if prices:
+            message += f"💰 Цены: {', '.join(prices)}\n\n"
+        else:
+            message += "Цены не найдены\n\n"
+
     await bot.send_message(
-        chat_id=401758093,
-        text="🤖 Бот запущен через GitHub Actions!"
+        chat_id=CHAT_ID,
+        text=message
     )
+
     await bot.session.close()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
