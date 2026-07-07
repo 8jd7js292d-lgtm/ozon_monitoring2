fb409c8d1bb68ddcf7c5cd5cdf9671433f13354d
8jd7js292d-lgtm <8jd7js292d@privaterelay.appleid.com>
2026-07-07 16:44:41 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,68 +1,39 @@
-import sqlite3
+import json
+import os
 
-
-DB_NAME = "ozon.db"
+FILE = "products.json"
 
 
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
-    cur = conn.cursor()
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        url TEXT UNIQUE,
-        name TEXT,
-        price REAL,
-        updated TEXT
-    )
-    """)
-
-    conn.commit()
-    conn.close()
+    if not os.path.exists(FILE):
+        with open(FILE, "w", encoding="utf-8") as f:
+            json.dump([], f)
 
 
 def add_product(url, name, price):
-    conn = sqlite3.connect(DB_NAME)
-    cur = conn.cursor()
+    products = get_products()
 
-    cur.execute("""
-    INSERT OR REPLACE INTO products
-    (url, name, price, updated)
-    VALUES (?, ?, ?, datetime('now'))
-    """,
-    (url, name, price))
+    products.append({
+        "url": url,
+        "name": name,
+        "price": price
+    })
 
-    conn.commit()
-    conn.close()
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(products, f, ensure_ascii=False, indent=2)
 
 
 def get_products():
-    conn = sqlite3.connect(DB_NAME)
-    cur = conn.cursor()
-
-    cur.execute("""
-    SELECT url, name, price
-    FROM products
-    """)
-
-    data = cur.fetchall()
-
-    conn.close()
-
-    return data
+    with open(FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 
 def update_price(url, price):
-    conn = sqlite3.connect(DB_NAME)
-    cur = conn.cursor()
+    products = get_products()
 
-    cur.execute("""
-    UPDATE products
-    SET price = ?, updated = datetime('now')
-    WHERE url = ?
-    """,
-    (price, url))
+    for p in products:
+        if p["url"] == url:
+            p["price"] = price
 
-    conn.commit()
-    conn.close()
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(products, f, ensure_ascii=False, indent=2)
